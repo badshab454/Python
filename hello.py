9f3aa15b142f3ea2a1b935f2ce6837bc4561fa48
badshab <badshab454@gmail.com>
2025-04-08 12:17:26 +0600

    python global variables

--- hello.py
+++ hello.py
@@ -1,45 +1,8 @@
-#Output variables exercise no 1
+#python global variables
 
-#Print the value stored in the student_name variable
+x = "awesome"
 
-# --- Variables for Exercises ---
-student_name = "Charlie"
-student_id = 9876
-course = "Introduction to Python"
-grade = 85.5
-is_active = True
-city = "Dhaka"
-num1 = 50
-num2 = 15
-product = "Book"
-price_per_item = 12.00
-quantity = 7
-# --- End of Setup Code ---
+def myfunc():
+    print("Python is " + x)
 
-# Using string concatenation (+ operator), print the sentence: "Student ID: 9876". Remember to convert the number if necessary.
-# Using string concatenation (+ operator), print the sentence: "Charlie is taking Introduction to Python". Use the student_name and course variables.
-# Using an f-string, print the sentence: "Student Charlie is active: True". Use the student_name and is_active variables.
-# Using an f-string, print the sentence: "Grade achieved: 85.5". Use the grade variable.
-# Using an f-string, print the sentence: "Charlie lives in Dhaka". Use the student_name and city variables.
-# Calculate the total_price by multiplying price_per_item by quantity. Then, using an f-string, print the sentence: "Total price for Book: $84.0". Use the product and the calculated total_price variables.
-
-# exercise 1 Print the value stored in the student_name variable.
-# print(student_name)
-
-# exercise 2 Print the value stored in the grade variable.
-
-# print(grade)
-
-# exercise 3 Print the student_name and student_id on the same line, separated by a space (use the comma method in print).
-
-# print(student_name, student_id)
-
-# exercise 4 Print the sentence: "Course: Introduction to Python" using the course variable and literal text. Use the comma method in print.
-
-print("Course:", course)
-
-
-
-
-
-
+myfunc()
