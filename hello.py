7ecf26c6f083721993d9031b3dfde520023964bc
badshab <badshab454@gmail.com>
2025-05-29 13:05:29 +0600

    hacker speak

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
 #python // 
-#check incensitive string comparison
+#hacker speak
 
-for i in range(1, 6):
-  print(i)
+def hacker_speak(txt):
+    replacements_map = {
+        "a": "4",
+        "e": "3",
+        "i": "1",
+        "o": "0",
+        "s": "5"
+    }
+    translation_table = str.maketrans(replacements_map)
+
+    return txt.translate(translation_table)
+
+print(hacker_speak("become a coder"))
