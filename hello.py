9905b0ee670ae3a336cc547fa9bb200e50af715d
badshab <badshab454@gmail.com>
2025-06-20 17:54:12 +0600

    sampe parity?

--- hello.py
+++ hello.py
@@ -3,7 +3,11 @@
 #Same parity
 
 def parity_analysis(num):
-    return num
+    string_num = str(num) #converted num int to str
+    sum = 0
+    for i in string_num:
+        sum += int(i)
+    return sum % 2 == num % 2
 
 print(parity_analysis(243))
 
