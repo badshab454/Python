b9cbd08dc23d4e1750e2adb13ac94dac36caa517
badshab <badshab454@gmail.com>
2025-06-23 18:18:46 +0600

    same parity

--- hello.py
+++ hello.py
@@ -2,4 +2,15 @@
 #Language fundamentals
 #Harvard CS50
 
-print("Hello, world!")
+#same parity?
+
+def parity_analysis(num):
+    string_num = str(num)
+    total_sum = 0
+    for i in string_num:
+        total_sum += int(i)
+    
+    return total_sum % 2 == num % 2
+
+
+print(parity_analysis(243))
