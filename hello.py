ff93a78f6a16e66d8e88309e24430d0aa5347295
badshab <badshab454@gmail.com>
2025-07-12 12:42:26 +0600

    word to number

--- hello.py
+++ hello.py
@@ -2,11 +2,19 @@
 #Loops
 #Num to dashes
 
-def num_to_dashes(num):
-    result = ""
-    for i in range(num):
-        result += "-"
-    return result
+def word(s):
+    word_to_num = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "zero": 0
+    }
+    return word_to_num[s]
 
-
-print(num_to_dashes(5))
+print(word("two"))
